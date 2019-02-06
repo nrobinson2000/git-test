442198129efd33d5550b3f06134683d06a804874
Nathan Robinson <nrobinson2000@me.com>
2019-02-06 13:23:57 -0500

    at 13:23 of 2019-02-06

--- src/git-test.ino
+++ src/git-test.ino
@@ -5,16 +5,19 @@
  * Date:
  */
 
- #include "metadata.h"
+#include "metadata.h"
+
+char gitData[100];
 
 void setup() {
-
+        snprintf(gitData, sizeof(gitData), "%s,%s", __GITBRANCH__, __GITCOMMIT__);
 }
 
 void loop() {
         static uint32_t msDelay = 0;
         if (millis() - msDelay >= 10000) {
                 Particle.publish("git-branch-name", __GITBRANCH__, 60, PRIVATE);
+                Particle.publish("git-data", gitData, 60, PRIVATE);
                 msDelay = millis();
         }
 }
